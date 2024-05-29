baabdb88c31634e7e9e86fde810bf79ca4252ead
YashRathod22 <yashrathod0226@gmail.com>
2024-05-29 14:55:14 +0530

    React Jobs

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Hero from '../components/Hero'
+import HomeCards from '../components/HomeCards'
+import JobsListing from '../components/JobsListing'
+import ViewAllJobs from '../components/ViewAllJobs'
+const HomePage = () => {
+  return (
+    <>
+      <Hero/>
+      <HomeCards/>
+      <JobsListing isHome={true} />
+      <ViewAllJobs/>
+    </>
+  )
+}
+
+export default HomePage
---
+++ src/pages/JobsPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import JobsListing from '../components/JobsListing'
+
+const JobsPage = () => {
+  return (
+    <>
+      <JobsListing/>
+    </>
+  )
+}
+
+export default JobsPage
